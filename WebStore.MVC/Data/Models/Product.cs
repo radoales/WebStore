4efcca5dabf51e2844bf7b9c8369e6cc723871b4
Radoslav Naydenov <radoales@users.noreply.github.com>
2021-01-19 13:40:43 +0100

    Remove StockItem

--- WebStore.MVC/Data/Models/Product.cs
+++ WebStore.MVC/Data/Models/Product.cs
@@ -8,8 +8,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public byte[] Image { get; set; }
-        public int StockItemId { get; set; }
-        public StockItem StockItem { get; set; }
         public IEnumerable<UserProducts> UserProducts { get; set; } = new List<UserProducts>();
     }
 }
